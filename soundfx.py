b80267fbdfdf49e2f7a6121096f69c74060c4a86
ChrisCraddock <30630937+ChrisCraddock@users.noreply.github.com>
2018-10-13 23:13:36 -0400

    Added sound effects for commands
    Chat can use '!' commands to play mp3 sound effects

---
+++ soundfx.py
@@ -0,0 +1,39 @@
+from playsound import playsound
+'''
+1) pip install soundplay
+2) Must include full path to folder
+   where mp3 is stored
+3) Reference in another file as 'import soundfx'
+'''
+
+
+
+def sounds0(command):
+    playsound('C:/Users/cmc03/Desktop/Python/Twitch/TTSBot/soundclips/wasted.mp3')
+
+def sounds1(command):
+    playsound('C:/Users/cmc03/Desktop/Python/Twitch/TTSBot/soundclips/Wrong.mp3')
+
+def sounds2(command):
+    playsound('C:/Users/cmc03/Desktop/Python/Twitch/TTSBot/soundclips/fart.mp3')
+
+def sounds3(command):
+    playsound('C:/Users/cmc03/Desktop/Python/Twitch/TTSBot/soundclips/worstpcbuilding.mp3')
+
+def sounds4(command):
+    playsound('C:/Users/cmc03/Desktop/Python/Twitch/TTSBot/soundclips/surprised.mp3')
+
+def sounds5(command):
+    playsound('C:/Users/cmc03/Desktop/Python/Twitch/TTSBot/soundclips/chewy.mp3')
+
+def sounds6(command):
+    playsound('C:/Users/cmc03/Desktop/Python/Twitch/TTSBot/soundclips/lawandorder.mp3')
+
+def sounds7(command):
+    playsound('C:/Users/cmc03/Desktop/Python/Twitch/TTSBot/soundclips/hankhillbwah.mp3')
+
+def sounds8(command):
+    playsound('C:/Users/cmc03/Desktop/Python/Twitch/TTSBot/soundclips/Concentration.mp3')
+
+def sounds9(command):
+    playsound('C:/Users/cmc03/Desktop/Python/Twitch/TTSBot/soundclips/gameover.mp3')
