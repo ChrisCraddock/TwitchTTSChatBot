d498aacb1c57af14c7e230a275170927fc28a208
ChrisCraddock <30630937+ChrisCraddock@users.noreply.github.com>
2018-10-13 23:50:15 -0400

    Update soundfx.py

--- soundfx.py
+++ soundfx.py
@@ -9,31 +9,31 @@ from playsound import playsound
 
 
 def sounds0(command):
-    playsound('C:/Users/cmc03/Desktop/Python/Twitch/TTSBot/soundclips/wasted.mp3')
+    playsound('C:/Users/---/Desktop/---/---/TTSBot/---/mp3name.mp3')
 
 def sounds1(command):
-    playsound('C:/Users/cmc03/Desktop/Python/Twitch/TTSBot/soundclips/Wrong.mp3')
+    playsound('C:/Users/---/Desktop/---/---/TTSBot/---/mp3name.mp3')
 
 def sounds2(command):
-    playsound('C:/Users/cmc03/Desktop/Python/Twitch/TTSBot/soundclips/fart.mp3')
+    playsound('C:/Users/---/Desktop/---/---/TTSBot/---/mp3name.mp3')
 
 def sounds3(command):
-    playsound('C:/Users/cmc03/Desktop/Python/Twitch/TTSBot/soundclips/worstpcbuilding.mp3')
+    playsound('C:/Users/---/Desktop/---/---/TTSBot/---/mp3name.mp3')
 
 def sounds4(command):
-    playsound('C:/Users/cmc03/Desktop/Python/Twitch/TTSBot/soundclips/surprised.mp3')
+    playsound('C:/Users/---/Desktop/---/---/TTSBot/---/mp3name.mp3')
 
 def sounds5(command):
-    playsound('C:/Users/cmc03/Desktop/Python/Twitch/TTSBot/soundclips/chewy.mp3')
+    playsound('C:/Users/---/Desktop/---/---/TTSBot/---/mp3name.mp3')
 
 def sounds6(command):
-    playsound('C:/Users/cmc03/Desktop/Python/Twitch/TTSBot/soundclips/lawandorder.mp3')
+    playsound('C:/Users/---/Desktop/---/---/TTSBot/---/mp3name.mp3')
 
 def sounds7(command):
-    playsound('C:/Users/cmc03/Desktop/Python/Twitch/TTSBot/soundclips/hankhillbwah.mp3')
+    playsound('C:/Users/---/Desktop/---/---/TTSBot/---/mp3name.mp3')
 
 def sounds8(command):
-    playsound('C:/Users/cmc03/Desktop/Python/Twitch/TTSBot/soundclips/Concentration.mp3')
+    playsound('C:/Users/---/Desktop/---/---/TTSBot/---/mp3name.mp3')
 
 def sounds9(command):
-    playsound('C:/Users/cmc03/Desktop/Python/Twitch/TTSBot/soundclips/gameover.mp3')
+    playsound('C:/Users/---/Desktop/---/---/TTSBot/---/mp3name.mp3')
